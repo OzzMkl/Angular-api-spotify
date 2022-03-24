3a53ffc3d33982a3b648c130b692c27eaba808a3
Oziel Pacheco <latikakm@gmail.com>
2022-03-24 10:26:15 -0600

    se termina vista general del artista

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-artist',
@@ -8,13 +9,38 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ArtistComponent implements OnInit {
 
+  //varibale para asignar el id del artista a buscar
   public artistId:string='';
+  //variable para asignar respuesta de servicios
+  public artist:any;
+  public albums:any;
 
-  constructor( private _activateRoute: ActivatedRoute) { }
+  constructor( private _activateRoute: ActivatedRoute,
+               private _spotifyService:SpotifyService) { }
 
   ngOnInit(): void {
+    //obtenemos el id del artista desde el navegador
     this._activateRoute.params.subscribe( params =>{
       this.artistId =  params['id'];
+
+      //obtenemos detalles el artista
+      this._spotifyService.getArtist(this.artistId).subscribe(
+        response => {
+           this.artist = response
+           //console.log(response);
+        },error => {
+          console.log(error);
+        });
+      //obtenemos albums del artista
+      this._spotifyService.getAllAlbums(this.artistId).subscribe(
+        response => {
+          this.albums = response.items;
+          console.log(this.albums);
+        },error =>{
+          console.log(error);
+        }
+      );
+
     })
       
   }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class SpotifyService {
 
   //autorizacion generada por spotify
-  private authorizationKey ='Bearer BQAhOA0DEVFXBohjNRBPrtP3KXTAFxKu0H5jSvcZRRkj_8S_oRfUgrgtyuihDQngdmsEIXBHcWhzIzwBHS8CRhQDn_quFyPAlJ-OM7t0kzYSbaDkIsjulGYBTui8RHbpOJ6K6hbDDb5gbVcejYkXjGSDrHq8xIoTAGQ';
+  private authorizationKey ='Bearer BQDxwwciCk9kHKf9tZiAw4zxLVLU8SvWzBXEh7UhQcJDgzFep594Wye7RDiSK2jpdmtquvL15crw1CcePPPfYpauvk10wDiAS4T1R4vOJjqF83QeRbcqfbqtcEFVOZNgyPoaIYAOtveIZeT5eSci-LOUs25R_OeVVwg';
   //creamos cabeceras
   private httpOptions = {
     headers : new HttpHeaders({
@@ -23,5 +23,14 @@ export class SpotifyService {
     let artistURL = `https://api.spotify.com/v1/search?q=${search}&type=artist`;
     //retornamos la respuesta
     return this._httpClient.get<any>(artistURL,this.httpOptions);
+  }
+  //obtener un solo artista
+  public getArtist(artistId:any):Observable<any>{
+    let artistIdURL='https://api.spotify.com/v1/artists/'+artistId;
+    return this._httpClient.get<any>(artistIdURL,this.httpOptions);
+  }
+  public getAllAlbums(artistId:any):Observable<any>{
+    let artistAlbumURL ='https://api.spotify.com/v1/artists/'+artistId+'/albums';
+    return this._httpClient.get<any>(artistAlbumURL,this.httpOptions);
   }
 }
